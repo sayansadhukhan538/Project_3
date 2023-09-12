19f8418cfd8df9e6774dc635dca94591fd24b3f1
Sayan Sadhukhan <77845946+sayansadhukhan538@users.noreply.github.com>
2023-09-12 12:40:01 +0530

    second commit

---
+++ src/components/Signup.jsx
@@ -0,0 +1,28 @@
+
+
+const Signup = () => {
+  return (
+    <div className="container">
+        <div className="card">
+            <h1>SIGN UP</h1>
+            <div className="input">
+                <input type="text"
+                placeholder=""
+                value={}
+                onChange={}
+                name=""
+                />
+                <input type="text"
+                placeholder=""
+                value={}
+                onChange={}
+                name=""
+                />
+                <button>SIGN UP</button>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+export default Signup
